89d48ae26f55baf492d4e10dd6fd572405929bc3
RutujaKavilkar <rutujabkavilkar19@gmail.com>
2026-04-02 14:12:52 +0530

    Added - CheckArrayis_SortedAndRotated_problem

---
+++ easy/CheckSortedAndRotatedArray.java
@@ -0,0 +1,13 @@
+class CheckSortedAndRotatedArray{
+    public boolean check(int[] arr) {
+        int count=0;
+        int n=arr.length;
+        for(int i=0;i<n;i++)
+        {
+            if(arr[i]>arr[(i+1)%n])
+            {
+                count++;
+            }
+        } return count<=1;
+    }
+}
